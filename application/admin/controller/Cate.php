5c9b0e59e96b23db5bfdd307c11fa6b192681c3e
ZKRS <2585122837@qq.com>
2017-04-28 21:27:49 +0800

    2017/4/26b

--- application/admin/controller/Cate.php
+++ application/admin/controller/Cate.php
@@ -18,6 +18,10 @@ class Cate extends \think\Controller
 {
     //分类列表
     public function cateList(){
+        $cate_select=db('cate')->select();//db助手函数
+        $cate_model=model('Cate');
+        $cate_list=$cate_model->getChildrenId($cate_select);
+        $this->assign('cate_list',$cate_list);//$cate_list分配给cate_list模版
         return view('cate/catelist');
     }
 }
---
+++ application/admin/model/Cate.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: hasee
+ * Date: 2017.4.28
+ * Time: 20:46
+ * 模型类名取名和对应操作的数据表名后缀一致
+ * 商品分类模型
+*/
+namespace app\admin\model;
+class Cate extends \think\Model
+{
+    //由父类id得到全部子类
+   public function getChildrenId($cate_list,$pid=0){
+       static $arr = array();
+       foreach ($cate_list as $key => $value){
+           if($value['cate_pid']==$pid){
+               $value['str']=str_repeat('----',$value['cate_level']);
+               $arr[]=$value;
+               $this->getChildrenId($cate_list,$value['id_cate']);
+           }
+       }
+        return $arr;
+
+   }
+
+
+}
