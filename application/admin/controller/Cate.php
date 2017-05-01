1d8e2931f3f9243d0cfe026a0946a973651521c8
ZKRS <2585122837@qq.com>
2017-05-01 21:06:39 +0800

    2017/4/26b

--- application/admin/controller/Cate.php
+++ application/admin/controller/Cate.php
@@ -17,29 +17,63 @@ namespace app\admin\controller;
 class Cate extends \think\Controller
 {
     //分类列表
-    public function cateList(){
-        $cate_select=db('cate')->select();//db助手函数
-        $cate_model=model('Cate');
-        $cate_list=$cate_model->getChildrenId($cate_select);
-        $cate_total=count($cate_list);
-        $page_class=new \app\admin\controller\Page($cate_total,3);
-        $show=$page_class->fpage();
-        $limit=$page_class->setLimit();
-        $limit=explode(',',$limit);
-        $list=array_slice($cate_list,$limit[0],$limit[1]);
-        $this->assign('show',$show);
-        $this->assign('cate_list',$cate_list);//$cate_list分配给cate_list模版
+    public function cateList()
+    {
+        $cate_select = db('cate')->select();//db助手函数
+        $cate_model = model('Cate');
+        $cate_list = $cate_model->getChildrenId($cate_select);
+        $cate_total = count($cate_list);
+        $page_class = new \app\admin\controller\Page($cate_total, 3);
+        $show = $page_class->fpage();
+        $limit = $page_class->setLimit();
+        $limit = explode(',', $limit);
+        $list = array_slice($cate_list, $limit[0], $limit[1]);
+        $this->assign('show', $show);
+        $this->assign('cate_list', $cate_list);//$cate_list分配给cate_list模版
         return view('cate/catelist');
     }
-    public function add(){
-        $cate_select=db('cate')->select();
-        $cate_model=model('Cate');
-        $cate_list=$cate_model->getChildrenId($cate_select);
-        $this->assign('cate_list',$cate_list);
+
+    public function add()
+    {
+        $cate_select = db('cate')->select();
+        $cate_model = model('Cate');
+        $cate_list = $cate_model->getChildrenId($cate_select);
+        $this->assign('cate_list', $cate_list);
         return view('cate/add');
     }
-    public function addhandle(){
-        $post=request()->post();
-        dump($post);
+
+    /**
+     * 添加分类提交的处理
+     */
+    public function addhandle()
+    {
+
+        $post = request()->post();
+        $cate_add = db('cate')->insert($post);
+        if ($cate_add) {
+            $this->success("分类添加成功", 'cate/catelist');
+        } else {
+            $this->error("分类添加失败", "cate/catelist");
+        }
+    }
+
+    /**
+     * 修改分类
+     */
+    public function update($id_cate)
+    {
+        if($id_cate==""){
+            $this->redirect('cate/catelist');
+        }
+        $cate_find=db('cate')->find($id_cate);
+        if($cate_find==""){
+            $this->redirect('cate/catelist');
+        }
+        $cate_select = db('cate')->select();
+        $cate_model = model('Cate');
+        $cate_list = $cate_model->getChildrenId($cate_select);
+        $this->assign('cate_list', $cate_list);
+        $this->assign('cate_find',$cate_find);
+        return view('cate/update');
     }
 }
--- application/admin/model/Cate.php
+++ application/admin/model/Cate.php
@@ -7,25 +7,24 @@
  * Time: 20:46
  * 模型类名取名和对应操作的数据表名后缀一致
  * 商品分类模型
-*/
+ */
+
 namespace app\admin\model;
 class Cate extends \think\Model
 {
     //由父类id得到全部子类
-   public function getChildrenId($cate_list,$pid=0){
-       static $arr = array();
-       foreach ($cate_list as $key => $value){
-           if($value['cate_pid']==$pid){
-               if($pid!=1){
-                   $value['str']=str_repeat('----',$value['cate_level']-1);
-               }else{
-                   $value['str']="";
-               }
-               $arr[]=$value;
-               $this->getChildrenId($cate_list,$value['id_cate']);
-           }
-       }
+    public function getChildrenId($cate_list, $pid = 0, $level = 1)
+    {
+        static $arr = array();
+        foreach ($cate_list as $key => $value) {
+            if ($value['cate_pid'] == $pid) {
+                $value['cate_level'] = $level;
+                $value['str'] = str_repeat('----', $value['cate_level'] - 1);
+                $arr[] = $value;
+                $this->getChildrenId($cate_list, $value['id_cate'],$level+1);
+            }
+        }
         return $arr;
 
-   }
+    }
 }
