f20e2c0335a59e2e9361707fc3bce0c96bda77e4
ZKRS <2585122837@qq.com>
2017-05-16 16:36:29 +0800

    2017/05/16

---
+++ application/index/controller/Goods.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hasee
+ * Date: 2017.5.14
+ * Time: 20:05
+ */
+
+namespace app\index\controller;
+
+
+class Goods extends \think\Controller
+{
+    public function goodslist($goods_pid="")
+    {
+        if($goods_pid == ""){
+            $this->redirect('index/index');
+        }
+        $goods_select = db('goods')->where("goods_status","eq",1)->where('goods_pid','eq',$goods_pid)->paginate(1);
+        dump($goods_select);die;
+        $this->assign('goods_select',$goods_select);
+        return view('goods/goodslist');
+    }
+}
