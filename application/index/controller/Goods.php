78b4ff2424a4501a03ce6d5fd56bea08b9a24950
ZKRS <2585122837@qq.com>
2017-05-24 16:21:53 +0800

    2017-5-24

--- application/index/controller/Goods.php
+++ application/index/controller/Goods.php
@@ -11,14 +11,18 @@ namespace app\index\controller;
 
 class Goods extends \think\Controller
 {
-    public function goodslist($goods_pid="")
+    public function goodslist($goods_pid = "")
     {
-        if($goods_pid == ""){
+        if ($goods_pid == "") {
             $this->redirect('index/index');
         }
-        $goods_select = db('goods')->where("goods_status","eq",1)->where('goods_pid','eq',$goods_pid)->paginate(1);
-        dump($goods_select);die;
-        $this->assign('goods_select',$goods_select);
+        $goods_select = db('goods')->where("goods_status", "eq", 1)->where('goods_pid', 'eq', $goods_pid)->paginate(1);
+        dump($goods_select);
+        die;
+        $this->assign('goods_select', $goods_select);
         return view('goods/goodslist');
     }
+}
+
+
 }
